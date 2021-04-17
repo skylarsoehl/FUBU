46419e63c4db0d4e2864921578db9e8087eaac7f
skylarsoehl <skysoehl@gmail.com>
2021-04-17 19:13:56 -0400

    added material ui dependency and global styles

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const font = "'Archivo', sans-serif";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#D5EEE6",
+      main: "#427966",
+      dark: "#2D5849",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#E5E7EE",
+      main: "#B0B3BC",
+      dark: "#8D919C",
+      contrastText: "#fff",
+    },
+    warning: {
+      main: "#DF5A45",
+      dark: "CB452F",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: font,
+    h1: {
+      fontSize: 33,
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: 22,
+    },
+  },
+});
+
+export default theme;
