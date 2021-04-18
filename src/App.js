17191e986fd01a46c064f2bb8f48049b788a2ea6
Rebekah Johnson <johnson.re@husky.neu.edu>
2021-04-18 03:02:59 -0400

    Adds schedule to app.js temporarily while solution for rendering each card in a schedule is pending

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import theme from "./theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
+import Schedule from './core/components/core-schedule.js';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Typography variant="h1">Hello World</Typography>
+      <Schedule></Schedule>
     </ThemeProvider>
   );
 }
